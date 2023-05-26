ac569458d0fd35444c873ccd015871e8eef2f157
xuejun <xuejunzhao0@gmail.com>
2023-05-26 13:10:16 +0800

    Recrusive classes update

---
+++ 19_Observer Pattern/build/CMakeFiles/Design_Patterns.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/agents.cpp" "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/build/CMakeFiles/Design_Patterns.dir/agents.cpp.o"
+  "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/main.cpp" "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/build/CMakeFiles/Design_Patterns.dir/main.cpp.o"
+  "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/observer.cpp" "/home/xuejun/C++Projects/Features/Design_Patterns/19_Observer Pattern/build/CMakeFiles/Design_Patterns.dir/observer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 19_Observer Pattern/build/CMakeFiles/Design_Patterns.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Design_Patterns.dir/main.cpp.o"
+  "CMakeFiles/Design_Patterns.dir/observer.cpp.o"
+  "CMakeFiles/Design_Patterns.dir/agents.cpp.o"
+  "Design_Patterns.pdb"
+  "Design_Patterns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Design_Patterns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
